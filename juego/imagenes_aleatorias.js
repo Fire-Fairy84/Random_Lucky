32c43fb957a2e4846bc26dee7d6dc4dc3d69d685
indiakka <78439130+indiakka@user.noreply.github.com>
2024-05-30 16:06:05 +0200

    imágenes cuadradas en máquina

--- juego/imagenes_aleatorias.js
+++ juego/imagenes_aleatorias.js
@@ -1,65 +1,3 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     const imagenCompis = document.getElementById("imagen_compis");
-//     const imagenIconos = document.getElementById("imagen_iconos");
-//     const imagenFamosos = document.getElementById("imagen_famosos");
-
-//     const compis = []; // Array para almacenar las imágenes de compis
-
-//     // Función para manejar el evento de cambio en el input de tipo file
-//     document.getElementById('archivos').addEventListener('change', (event) => {
-//         const archivos = event.target.files;
-//         const tipo = document.querySelector('option:checked').value;
-//         // Leer cada archivo seleccionado y agregarlo al array correspondiente
-//         for (let i = 0; i < archivos.length; i++) {
-//             const reader = new FileReader();
-//             reader.onload = function (e) {
-//                 switch (tipo) {
-//                     case 'compis':
-//                         compis.push(e.target.result); // Agregar la imagen leída al array
-//                         imagenCompis.src = e.target.result; // Mostrar la imagen cargada
-//                         break;
-//                     case 'iconos':
-//                         // Agregar lógica para manejar las imágenes de iconos
-//                         break;
-//                     case 'famosos':
-//                         // Agregar lógica para manejar las imágenes de famosos
-//                         break;
-//                     default:
-//                         break;
-//                 }
-//             }
-//             reader.readAsDataURL(archivos[i]); // Leer el archivo como una URL de datos
-//         }
-//     });
-// });
-
-//   const simbolos = [
-//     "anillo.png",
-//     "beso.png",
-//     "cama.png",
-//     "cuchillo.png",
-//     "veneno.png",
-//     "viaje.png",
-//   ];
-//   const famosos = [
-//     "Angelina.png",
-//     "Danny.png",
-//     "David.png",
-//     "Donatella.png",
-//     "JuanCar.png",
-//     "Lady.png",
-//     "Meryl.png",
-//     "Pedro.png",
-//     "Santiago.png",
-//     "Vigo.png",
-//   ];
-
-//   // Función para obtener una imagen aleatoria del array dado
-//   function imagenesAleatorias(imagenes) {
-//     const indiceAleatorio = Math.floor(Math.random() * imagenes.length);
-//     return imagenes[indiceAleatorio];
-//   }
-
 let compis = [];
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -86,11 +24,11 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     // Mostrar imágenes aleatorias
-    imagen2.src = "../Imagenes/iconos/" + imagenesAleatorias(simbolos);
+    imagen2.src = "../Imagenes/iconos/" + imagenesAleatorias(iconos);
     imagen3.src = "../Imagenes/famosos/" + imagenesAleatorias(famosos);
   });
 
-  const simbolos = [
+  const iconos = [
     "anillo.png",
     "beso.png",
     "cama.png",

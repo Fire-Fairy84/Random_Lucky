1c39d560f1c59ae6c069e2cec2e9784880b29759
indiakka <78439130+indiakka@user.noreply.github.com>
2024-05-31 13:13:01 +0200

    funcionalidad onclick de imagen funcionando a falta de posicionar la imagen

--- juego/imagen_onclick.js
+++ juego/imagen_onclick.js
@@ -1,30 +1,33 @@
 // Función para cambiar la imagen
+const audio = document.getElementById("miAudio");
+
 function cambiarImagen() {
   var imagen = document.getElementById("maquina");
   // Aquí cambia la ruta de la imagen por la nueva ruta
-  imagen.src = "../Imagenes/.jpg";
+  imagen.src = "../Imagenes/complementos/maquinaAbajo.svg";
+  audio.play();
 }
 
-// Agrega un event listener para el clic en la imagen
-document
-  .getElementById("imagenCambiante")
-    .addEventListener( "click", cambiarImagen );
+// // Agrega un event listener para el clic en la imagen
+// document
+//   .getElementById("imagenCambiante")
+//     .addEventListener( "click", cambiarImagen );
   
 
-    const imagen = document.getElementById("maquinaArriba");
-    const audio = document.getElementById("miAudio");
+    // const imagen = document.getElementById("maquinaArriba");
+    // const audio = document.getElementById("miAudio");
 
-    // Añadir evento de clic a la imagen
-    imagen.addEventListener("click", () => {
-      // Cambiar la fuente de la imagen
-      imagen.src = "maquinaAbajo.svg";
+    // // Añadir evento de clic a la imagen
+    // imagen.addEventListener("click", () => {
+    //   // Cambiar la fuente de la imagen
+    //   imagen.src = "maquinaAbajo.svg";
 
-      // Reproducir el audio
-      audio.play();
-    } );
+    //   // Reproducir el audio
+    //   audio.play();
+    // } );
     
-    const cambiarImagen = () => {
-      document.getElementById("maquinaArriba").src =
-        "./imagenes/maquinaAbajo.jpg";
-    };
+    // const cambiarImagen = () => {
+    //   document.getElementById("maquinaArriba").src =
+    //     "./imagenes/maquinaAbajo.jpg";
+    // };
 
